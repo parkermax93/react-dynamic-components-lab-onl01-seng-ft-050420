b04fdaa71d3d55a0a05ee2dcddb9b7741d21f93a
parkermax93 <64799478+parkermax93@users.noreply.github.com>
2021-02-22 16:17:13 -0800

    Fix Colorbox spacing

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -5,20 +5,20 @@ export default class ColorBox extends Component {
   state = {
     todos: [
       
-    ]
+     ]
   }
 
-  render() {
-    if(this.props.opacity >= 0.2) {
-    return (
-      <div className="color-box" style={{opacity: this.props.opacity}}>
-        <ColorBox opacity ={this.props.opacity - 0.1} />
-      </div>
-    )
-  } else {
-    return null
+   render() {
+     if(this.props.opacity >= 0.2) {
+      return (
+        <div className="color-box" style={{opacity: this.props.opacity}}>
+          <ColorBox opacity ={this.props.opacity - 0.1} />
+       </div>
+      )
+    } else {
+      return null
+    }
   }
-}
 
 }
 
